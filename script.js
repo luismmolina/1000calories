1369b71932468dd93422db6472e1aad1b34d0467
Luis Martinez <luismmolina@gmail.com>
2023-09-04 13:30:14 -0600

    PWA

--- script.js
+++ script.js
@@ -13,6 +13,16 @@ function updateCalories() {
     }
 }
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(function(error) {
+        console.log('Service Worker registration failed:', error);
+      });
+  }
+  
 // Initialize and update every minute
 updateCalories();
 setInterval(updateCalories, 1000);
